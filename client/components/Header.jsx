5ad72d7db70df98d9c694b949279bb5f7d4c8df5
haseeb1965 <haseebsiddiqui1965@gmail.com>
2023-03-10 00:29:03 +0500

    updated Header.jsx

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import Search from '@mui/icons-material/Search';
+import React from "react";
+import Search from "@mui/icons-material/Search";
+import Menu from '@mui/icons-material/Menu';
+import PermIdentity from '@mui/icons-material/PermIdentity';
 const Header = () => {
   return (
-   <header className='p-4 flex'>
-      <a href="" className='flex flex-row gap-1 items-center'>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8 -rotate-90">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-</svg>
-       <span className='font-bold text-xl'>airbnb</span>
+    <header className="p-4 flex justify-between">
+      <a href="" className="flex flex-row gap-1 items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="w-8 h-8 -rotate-90"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+          />
+        </svg>
+        <span className="font-bold text-xl">airbnb</span>
       </a>
-      <div className='flex'>
-         <div>Anywhere</div>
-         <div>Any week</div>
-         <div>Add guests</div>
-         <button>
-            <Search />
-         </button>
+      <div className="flex gap-2 border border-gray-300 shadow-md shadow-gray-300 rounded-full py-2 px-4">
+        <div>Anywhere</div>
+        <div className="border border-l border-gray-300"></div>
+        <div>Any week</div>
+        <div className="border border-l border-gray-300"></div>
+        <div>Add guests</div>
+        <button className="bg-primary text-white p-1 rounded-full ">
+          <Search className="!w-6 !h-6" />
+        </button>
       </div>
-   </header>
-  )
-}
 
-export default Header
+      <div className="flex gap-2 border border-gray-300 shadow-md shadow-gray-300 rounded-full py-2 px-4">
+         <Menu />
+        <PermIdentity />
+        {/*Continue....*/}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
